e84bb0cedd8e48bde95fe47addd4bbd00f08c4d9
Andres Luque <sistemas@biccofarms.com>
2015-02-04 16:04:32 -0500

    Se agregan titulos e imagen de perfil

--- index.php
+++ index.php
@@ -2,9 +2,14 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Ejemplo de Git</title>
 </head>
 <body>
-	
+	<h1>Ejemplo de GIT</h1>
+	<h2>Sistema de Control de Versiones</h2>
+	<p>
+		<img src="https://pbs.twimg.com/profile_images/490231963182055424/GpvVDG4r.jpeg" width="200">
+		Nelson Andres Luque Gonzalez
+	</p>
 </body>
 </html>
